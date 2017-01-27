f9e34db431e8e7ee466d3b06f0c00ac135f7371b
Epik17 <floyd17@yandex.ru>
2017-01-27 21:07:09 +0400

    trajectoryPlot simplified

--- DEoverloads.m
+++ DEoverloads.m
@@ -98,18 +98,9 @@ maneuver[___]:=$Failed
 
 
 (* ::Input::Initialization:: *)
-ClearAll@trajPlotArgs 
-trajPlotArgs[manevrresult_?manevrQ]:={#[t]&/@({x,y,z}/.manevrresult),tFinal[manevrresult]}
-trajPlotArgs[___]:=$Failed
-
-
-(* ::Input::Initialization:: *)
 ClearAll@trajectoryPlot
-trajectoryPlot[{input_,tfin_}]:=ParametricPlot3D[input,{t,0,tfin},PlotRange->Full,AxesLabel->{"x","y","z"}]
-trajectoryPlot[manevrresult_?manevrQ]:=trajectoryPlot[trajPlotArgs[manevrresult]]
+trajectoryPlot[manevrresult_?manevrQ]:=ParametricPlot3D[#[t]&/@({x,y,z}/.manevrresult),{t,0,tFinal[manevrresult]},PlotRange->Full,AxesLabel->{"x","y","z"},PlotStyle->Thickness[0.0075]]
 trajectoryPlot[___]:=$Failed
-
-(* \:0438\:0441\:043f\:043e\:043b\:044c\:0437\:0443\:0435\:0442\:0441\:044f \:0444\:0443\:043d\:043a\:0446\:0438\:044f-\:043f\:0440\:043e\:043a\:043b\:0430\:0434\:043a\:0430 trajPlotArgs. \:041d\:0430\:043f\:0440\:044f\:043c\:0443\:044e, \:0441 \:043f\:0435\:0440\:0435\:0434\:0430\:0447\:0435\:0439 \:0432 ParametricPlot3D \:0440\:0435\:0448\:0435\:043d\:0438\:044f \:0432 \:0432\:0438\:0434\:0435 \:0441\:043f\:0438\:0441\:043a\:0430 \:043f\:0440\:0430\:0432\:0438\:043b, ParametricPlot3D \:043f\:043e\:0447\:0435\:043c\:0443-\:0442\:043e \:043d\:0435 \:0440\:0430\:0431\:043e\:0442\:0430\:0435\:0442 \:043f\:0440\:0438 \:0434\:043e\:0431\:0430\:0432\:043b\:0435\:043d\:0438\:0438 \:043e\:043f\:0446\:0438\:0439 \:043a \:0433\:0440\:0430\:0444\:0438\:043a\:0443; \:044f\:0432\:043d\:043e \:043a\:0430\:043a\:043e\:0439-\:0442\:043e \:0433\:043b\:044e\:043a \:043c\:0430\:0442\:0435\:043c\:0430\:0442\:0438\:043a\:0438 *)
 
 
 (* ::Input::Initialization:: *)
