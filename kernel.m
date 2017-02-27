ab783be748a2cc34dd1ad824c9203529c2f62ef3
Epik17 <floyd17@yandex.ru>
2017-02-27 21:53:20 +0400

    minor

--- kernel.m
+++ kernel.m
@@ -360,8 +360,10 @@ ErrorChecking`setConsistencyChecks[join,"Valid syntax for public version of the 
 ClearAll[idetails,details]
 SetAttributes[idetails,HoldFirst]
 SetAttributes[details,HoldFirst]
-
-idetails[man_,tags_]:=ReleaseHold[(Reap[man;,tags][[2]])]
+idetails::reaperror="Reap error. It is possible that some of maneuvers parts names are the same";
+idetails[man_,tags_]:=Module[{res=ReleaseHold[(Reap[man,tags])]},If[numberOfparts@res[[1]]=!=Length@res[[2,1]],Message[idetails::reaperror]];
+res[[2]]
+]
 
 details[man_,tags_:{tt[_],localtt[_],xx[_],yy[_],zz[_],\[Theta]\[Theta][_],\[Theta]dot[_],\[Psi]\[Psi][_],\[Psi]dot[_],gam[_],ny[_],nyavaliable[_],nx[_],nxavaliable[_],VV[_],a[_]}]:=
 With[
@@ -398,17 +400,4 @@ Length@intfuns[[1,2,1]],
 ]
 
 
-(* ::Input::Initialization:: *)
-ClearAll[idetails,details]
-SetAttributes[idetails,HoldFirst]
-SetAttributes[details,HoldFirst]
-idetails::reaperror="Reap error. It is possible that not all parts of maneuver have names or some of the names are the same";
-idetails[man_,tags_]:=Module[{res=ReleaseHold[(Reap[man,tags])]},If[numberOfparts@res[[1]]=!=Length@res[[2,1]],Message[idetails::reaperror]];
-res[[2]]
-]
 
-details[man_,tags_:{tt[_],localtt[_],xx[_],yy[_],zz[_],\[Theta]\[Theta][_],\[Theta]dot[_],\[Psi]\[Psi][_],\[Psi]dot[_],gam[_],ny[_],nyavaliable[_],nx[_],nxavaliable[_],VV[_],a[_]}]:=
-With[
-{completedtags=DeleteDuplicates@Join[{tt[_],localtt[_]},tags]},
-{completedtags/.{x_[_]:>x}}~Join~Flatten[(Sort[#,#1[[1]]<#2[[1]]&]&/@Transpose/@Transpose@idetails[man,completedtags]),1]
-]
